5d49f892e2866a3986e483008aac0c64c6703c0b
Maridon Romney <mkromney@gmail.com>
2023-05-25 21:12:10 -0600

    Added text to HTML

--- Assets/script.js
+++ Assets/script.js
@@ -9,7 +9,10 @@ var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 function addCityToHistory(cityName) {
   searchHistory.push(cityName);
   localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-};
+  console.log(localStorage)
+  };
+
+  
 
 // Function to clear the search history
 function clearSearchHistory(event) {
@@ -30,29 +33,29 @@ function searchCity(event) {
     // Performs the search here. //
 
     searchAPI(cityName);
-    // Add the city to the search history
+    // Adds the city to the search history. //
     addCityToHistory(cityName);
 
-    // Clear the input field
+    // Clears the input field after a search is conducted. //
     input.value = "";
     };
   };
 
 
-// The concatenation below makes it so you can search any city. //
+// The concatenation below makes it so you can search by city instead of by coordinates and displays Fahrenheit (imperial temp). //
 function searchAPI(cityName) {
   var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=40e8f4e7cce1a052fa34799e8403ed4a&units=imperial"
 
-  // Might 'get' be better here? //
+    // Json method on the response. //
   fetch(url).then(function(response){
-    return response.json() // json method on the response //
+    return response.json() 
 
   })
     .then(function(data){
-      //clear the data  here //
+      //Sets a variable that clears the data when called below. //
       clearWeatherData()
     
-
+      // This for loop makes it possible to iterate over and print data from the Open Weather API. Due to the standard 5-day Forecast being unavailable for free I used math logic to make the 5-day 3-hour forecast work instead. This means the forecast displays the future outside the paywall. //
       console.log(data)
       $(".grid-item").each(function(i, el) {
         var $el = $(el);
@@ -69,7 +72,7 @@ function searchAPI(cityName) {
         $currentDateTime.text("Date/Time: "+data.list [2+(i*8)].dt_txt);
         $currentDateTime.appendTo($el);
        
-        //create an image class, instead of a div google fonts, converts conditions to google.//
+        //Creates an image class, instead of a div google fonts, converts conditions to google.//
         var $icon = $("<img/>");
         $icon.addClass("forecast-icon");
         $icon.attr("src", "http://openweathermap.org/img/w/" + data.list[2+(i*8)].weather[0].icon + ".png");
@@ -93,10 +96,10 @@ function searchAPI(cityName) {
       });
     });
   }
-// debugger; // **Remove when done with this** //
+// debugger; // **Remove when done with this.** //
   
 
-// Loads the search history onto the page
+// Loads the search history onto the page - could not figure out how to make this happen after clicking "search". //
 window.addEventListener("DOMContentLoaded", function () {
   var cityList = document.getElementById("cityList");
 
@@ -105,18 +108,18 @@ window.addEventListener("DOMContentLoaded", function () {
     listItem.textContent = city;
     cityList.appendChild(listItem);
 
-  // Add click event listener to each list item
+  // Adds click event listener to each list item. //
   listItem.addEventListener("click", function () {
     // Retrieve the city name from the clicked list item
     var cityName = this.textContent;
 
-    // Call the searchAPI function with the clicked city name
+    // Calls the searchAPI function with the clicked city name. //
     searchAPI(cityName);
   });  
   });
 });
 
-// Attach event listeners to the search button and clear search button
+// Attaches event listeners to the search button and clear search button. //
 var searchButton = document.getElementById("searchButton");
 searchButton.addEventListener("click", function(event) { searchCity(event); });
 
@@ -124,7 +127,7 @@ var clearSearchButton = document.getElementById("clearSearchButton");
 clearSearchButton.addEventListener("click", function(event) { clearSearchHistory(event) });
 
 
-
+// This function calls the "clear weather data" for each data item in the for loop beginning on line 59. //
 function clearWeatherData () {
   $("div.forecast-city").remove();
   $("div.forecast-date").remove();
